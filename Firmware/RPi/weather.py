c928fb9581a0f1c39e3542c662faafdc1f81850c
gedonawais <muhammad.awais@gedonsoft.de>
2026-07-15 19:11:42 +0200

    Update weather.py

--- Firmware/RPi/weather.py
+++ Firmware/RPi/weather.py
@@ -359,7 +359,7 @@ try:
     while True:
         if GPIO.input(SHUTDOWN_FROM_ESP32) == GPIO.HIGH:
             time.sleep(1)
-            print(""Got Signal from ESP32 after sending Image,Uploading Logs and Going to SHUT DOWN")
+            print("Got Signal from ESP32 after sending Image,Uploading Logs and Going to SHUT DOWN")
             logging.info("Got Signal from ESP32 after sending Image,Uploading Logs and Going to SHUT DOWN")
             logging.info("===================================================================================\n")
             uploadLogs()

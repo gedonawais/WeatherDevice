bf74840f668ab239a6b8bb85c42dfcf1fdf82193
Muhammad Awais <muhammad.awasi@gedonsoft.de>
2026-08-03 13:40:20 +0200

    Update weather.py

--- Firmware/RPi/weather.py
+++ Firmware/RPi/weather.py
@@ -393,11 +393,11 @@ try:
                 logging.error(f"Unexpected FTP response: {resp1}, {resp2}, {resp3}")
 
         except error_perm as e:
-            print("Permission or FTP error")
-            logging.error(f"Permission or FTP error: {e}")
+            print(f"Upload Failed {type(e).__name__}: {e}")
+            logging.error(f"Upload failed: {type(e).__name__}: {e}")
         except Exception as e:
-            print(f"Upload Failed {e}")
-            logging.error(f"Upload failed: {e}")
+            print(f"Upload Failed {type(e).__name__}: {e}")
+            logging.error(f"Upload failed: {type(e).__name__}: {e}")
         finally:
             try:
                 ftp.quit()

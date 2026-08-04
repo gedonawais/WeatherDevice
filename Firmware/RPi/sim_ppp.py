00e96a6cc5ad0e42488d6b1cae895a11c1b57962
Muhammad Awais <muhammad.awasi@gedonsoft.de>
2026-08-04 11:27:04 +0200

    Update sim_ppp.py

--- Firmware/RPi/sim_ppp.py
+++ Firmware/RPi/sim_ppp.py
@@ -27,14 +27,16 @@ def ensure_dns():
             text=True
         )
         if result.returncode == 0:
-            print("Copied /etc/ppp/resolv.conf to /etc/resolv.conf")
+            print("DNS copied successfully")
             log_no_time("Copied /etc/ppp/resolv.conf to /etc/resolv.conf")
             return True
         else:
-            print(f"Failed to copy DNS config: {result.stderr}")
+            print("DNS copy failed:", result.stderr)
             log_no_time(f"Failed to copy DNS config: {result.stderr}")
             return False
+        
     except Exception as e:
+        print("DNS copy exception:", e)
         log_no_time(f"Failed to copy DNS config: {e}")
         return False
     

4b13d19744ff870259cfe5caa079bd8624c0aeb8
gedonawais <muhammad.awais@gedonsoft.de>
2026-08-11 16:27:52 +0200

    Update weather.py

--- Firmware/RPi/weather.py
+++ Firmware/RPi/weather.py
@@ -517,9 +517,7 @@ try:
     #------------------- Get Frame Rate from Server -----------------
     newFrameRate = getFrameRate(config)
     if newFrameRate is not None and newFrameRate!= config.get("frameRate"):
-        config["frameRate"] = newFrameRate
-        save_config(config)
-        logging.info(f"Frame rate updated to {newFrameRate} from server")
+        logging.info(f"Frame rate {newFrameRate} fetched from server for next cycle")
     else:
         logging.info(f"Frame rate remains {config.get('frameRate')}")
 

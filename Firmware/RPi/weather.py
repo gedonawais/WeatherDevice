b01dc29121c2b80d9a8d72b77b3b25df764c518b
gedonawais <muhammad.awais@gedonsoft.de>
2026-08-26 17:46:51 +0200

    Update weather.py

--- Firmware/RPi/weather.py
+++ Firmware/RPi/weather.py
@@ -469,7 +469,7 @@ def upload_low_battery_status(config, battery_value):
                 "location": config.get("location", ""),
                 "battery": battery_value,
                 "sleepReason": "low_battery",
-                "sleepMinutes": 60,
+                "sleepMinutes": 120,
             },
             timeout=30,
         )
